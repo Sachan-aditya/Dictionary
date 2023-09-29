8edcfed53aa974939c3c2657b6cb00948f7dd8b6
adityasachan24@gmail.com <adityasachan24@gmail.com>
2023-09-29 23:27:59 +0530

    working on

--- src/App.svelte
+++ src/App.svelte
@@ -11,7 +11,16 @@
 	loading=true;
 	word=null;
 	try{
-   let res= await
+   let res= await fetch("https://api.dictionaryap.dev/api/v2/entries/en/"+word);
+   let data=await res.json();
+   if(Array.isArray(data))
+   {
+	wordData=data[0];
+   }
+   else
+   {
+	wordData="No Result"
+   }
 	}
 	catch(err){
 		loading =false;
@@ -21,4 +30,17 @@
 
 </script>
 <style></style>
+<div class="header">Dictionary App</div>
+<div class="center">
+<div class="form">
+	<div class="from-group">
+	<label for="Search Word"></label>
+		<input type="text" placeholder="Type word here" bind:value={word}/>
+	
+	</div>
+	<div class="form-group">
+		<button on:click={searchWord}>Search</button>
+	</div>
+</div>
+</div>
 <main></main>
