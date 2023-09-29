3cb9e992de6d554d0655d09e13a558031a5d3942
adityasachan24@gmail.com <adityasachan24@gmail.com>
2023-09-29 23:01:12 +0530

    first commit

---
+++ src/Loader.svelte
@@ -0,0 +1,22 @@
+<style>
+    .loader {
+        position: relative;
+        padding: 40px;
+        width: 100%;
+        height: 100%;
+    }
+    .loader:before{
+        content: "";
+        position:absolute;
+        top: 50%;
+        left:50%;
+        transform: translate(-50%,-50%);
+        width:50px;
+        height: 50px;
+        border: 2px solid #111;
+        border-left:2px solid transparent;
+
+    }
+
+</style>
+<div>class="loader"</div>
