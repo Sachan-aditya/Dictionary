a39866ddd95a6705e57e44dcbe1aed4b87d78d41
adityasachan24@gmail.com <adityasachan24@gmail.com>
2023-10-01 14:22:40 +0530

    updated

--- src/WordData.svelte
+++ src/WordData.svelte
@@ -5,10 +5,16 @@
 <style></style>
 <div class="word-data">
     {
-        #if wordData.phonerics
+        #if wordData.phonetics
     }
     <div class="audios">
-        
+    {#each wordData.phonetics as data}
+    <div class="audios">
+        <p>{
+            data.text}
+            <i class ="fa fa .play-circle" on on:click={()=>playAudio(data.audio)}></i></p>
+    </div>
+    {/each}
     </div>
     {
         /if
