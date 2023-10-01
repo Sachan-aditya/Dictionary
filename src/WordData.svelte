24bbd353b047e6d2da329dffe3de756ab6c93f4a
adityasachan24@gmail.com <adityasachan24@gmail.com>
2023-10-01 14:17:08 +0530

    updated

---
+++ src/WordData.svelte
@@ -0,0 +1,16 @@
+<script>
+
+
+</script>
+<style></style>
+<div class="word-data">
+    {
+        #if wordData.phonerics
+    }
+    <div class="audios">
+        
+    </div>
+    {
+        /if
+    }
+</div>
